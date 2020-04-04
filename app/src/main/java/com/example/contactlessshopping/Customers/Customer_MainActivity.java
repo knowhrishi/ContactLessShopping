cb86647e99e8d8bd0a3ee9164a2f1fada6358208
bhosaleharshita <harshitabhosale5600@gmail.com>
2020-04-04 10:23:09 +0530

    latitude longitude for customer -added

--- app/src/main/java/com/example/contactlessshopping/Customers/Customer_MainActivity.java
+++ app/src/main/java/com/example/contactlessshopping/Customers/Customer_MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.contactlessshopping.Customers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,20 +26,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.contactlessshopping.R;
+import com.example.contactlessshopping.Shops.ShopRegistration;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class Customer_MainActivity extends AppCompatActivity {
 
-
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
    ProgressBar progressBar;
-
-
     RecyclerView  shoplist;
+    LocationManager locationManager;
+    String LATITUDE,LONGITUDE,LOC;
 
     private FirebaseFirestore db;
     private FirestoreRecyclerAdapter adapter;
@@ -42,6 +61,19 @@ public class Customer_MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer__main);
         shoplist=findViewById(R.id.shop_list);
         progressBar=findViewById(R.id.progress_bar);
+
+        if (ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    Customer_MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION
+            );
+        }
+
+        getCurrentLocation();
+
+
 
         init();
         getshopList();
@@ -127,6 +159,72 @@ public class Customer_MainActivity extends AppCompatActivity {
         adapter.stopListening();
     }
 
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            getCurrentLocation();
+        } else {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getCurrentLocation()  {
+
+
+        final LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.getFusedLocationProviderClient(Customer_MainActivity.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(Customer_MainActivity.this)
+                                .removeLocationUpdates(this);
+                        if (locationResult != null && locationResult.getLocations().size() > 0) {
+                            int lastestLocationIndex = locationResult.getLocations().size() - 1;
+                            double latitude =
+                                    locationResult.getLocations().get(lastestLocationIndex).getLatitude();
+                            double longitude =
+                                    locationResult.getLocations().get(lastestLocationIndex).getLongitude();
+
+
+                            LATITUDE = String.format(String.valueOf(latitude));
+                            LONGITUDE = String.format(String.valueOf(longitude));
+                            LOC=String.format(Locale.US, "%s -- %s", LATITUDE, LONGITUDE);
+                            Log.d("Location:",LATITUDE);
+
+
+                          /*  Geocoder geocoder;
+                            List<Address> addresses;
+                            geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+
+
+                            addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+
+                            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                            String city = addresses.get(0).getLocality();
+                            String state = addresses.get(0).getAdminArea();
+                            String country = addresses.get(0).getCountryName();
+                            String postalCode = addresses.get(0).getPostalCode();
+                            String knownName = addresses.get(0).getFeatureName();*/
+
+                            Log.d("Loc::",LOC);
+
+                        }
+                    }
+                }, Looper.getMainLooper());
+
+    }
 
 
 
